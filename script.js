a8439161a35c7aec72beacb633f546a2cc6db9c5
Lorelai Knutti <virgoknight2@gmail.com>
2022-03-25 12:53:58 -0400

    got rid of tag hiding img

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const h2 = document.createElement("h2");
 h2.textContent = "this content addded by JavaScript";
 document.querySelector("body").appendChild(h2);
+function displayImg() {
+    Selfie.displayImg
+    Mama.displayImg
+}
