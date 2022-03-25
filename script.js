3b085d4f5b38838de8b69cd62f53ccce37f6426d
Lorelai Knutti <virgoknight2@gmail.com>
2022-03-25 13:24:58 -0400

    updates made to some base code

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "this content addded by JavaScript";
+h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
-function displayImg() {
-    Selfie.displayImg
-    Mama.displayImg
-}
