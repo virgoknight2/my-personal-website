ebe3804ef14072eaede74ffb8ac25f907edbf619
Lorelai Knutti <virgoknight2@gmail.com>
2022-03-25 14:04:30 -0400

    updated code to hopefully work

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
-const input = document.getElementById('input');
 
-function setHideImages(mama,selfie, visible) {
+const input = document.getElementById('body');
+
+function setHideImages( visible) {
   var img= document.getElementsById('mama');
   var img1= document.getElementById('selfie');
   img.style.visibility = (visible ? 'visible' : 'hidden');
   img1.style.visibility = (visible ? 'visible' : 'hidden');
 }
 
-input.addEventListener('click', setHideImages);
+input.addEventListener('click', setHideImages(false));
