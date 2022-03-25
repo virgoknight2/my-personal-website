df787b37b09295d58bb5e985621dac39a84421db
Lorelai Knutti <virgoknight2@gmail.com>
2022-03-25 13:53:09 -0400

    script to hide images

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+const input = document.getElementById('input');
+
+function setHideImages(mama,selfie, visible) {
+  var img= document.getElementsById('mama');
+  var img1= document.getElementById('selfie');
+  img.style.visibility = (visible ? 'visible' : 'hidden');
+  img1.style.visibility = (visible ? 'visible' : 'hidden');
+}
+
+input.addEventListener('click', setHideImages);
