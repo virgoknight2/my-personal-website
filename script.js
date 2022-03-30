0472b985c83633226e166512189683203ec025ef
Lorelai Knutti <virgoknight2@gmail.com>
2022-03-30 15:09:27 -0400

    changed button function to  change text color

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
 
-const input = document.getElementById('body');
+const btn = document.getElementById('btn');
 
+btn.addEventListener('click', function onClick(event) {
+  // 👇️ Change text color globally
+  document.body.style.color = 'white';
+});
