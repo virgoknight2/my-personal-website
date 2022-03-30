ea4e9c63805706f2c468d61067853d141655e337
Lorelai Knutti <virgoknight2@gmail.com>
2022-03-30 15:00:38 -0400

    removed code hiding images

--- script.js
+++ script.js
@@ -4,11 +4,3 @@ document.querySelector("body").appendChild(h2);
 
 const input = document.getElementById('body');
 
-function setHideImages(visible) {
-  var img= document.getElementById('mama');
-  var bunt= document.getElementById('selfie');
-  img.style.visibility = (visible ? 'visible' : 'hidden');
-  bunt.style.visibility = (visible ? 'visible' : 'hidden');
-}
-
-input.addEventListener('click', setHideImages(false));
